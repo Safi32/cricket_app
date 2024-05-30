3aa1661f5b4af16ea5baf5235a5ef7cee813c067
Safi32 <138612632+Safi32@users.noreply.github.com>
2024-05-30 23:16:50 +0500

    photoUrl

--- lib/Screens/my_profile.dart
+++ lib/Screens/my_profile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cricket_app/Screens/auth_screen/login.dart';
+import 'package:cricket_app/utils/colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -106,7 +107,8 @@ class _UserProfileState extends State<UserProfile> {
                     SizedBox(height: 20),
                     CircleAvatar(
                       radius: 50,
-                      backgroundColor: Colors.black,
+                      backgroundColor: primaryColor,
+                      backgroundImage: NetworkImage(user?['photoUrl'] ?? ""),
                     ),
                   ],
                 ),
